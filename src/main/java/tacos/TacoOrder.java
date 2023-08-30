d636ab853ca580fd4c38f3c1425ce5eb05462085
OKIS <67818930+okis-okis@users.noreply.github.com>
2023-08-30 15:54:50 +0300

    Worked with MongoDB
    Project have some errors

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -4,33 +4,28 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.cassandra.core.mapping.Column;
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Table;
-
-import com.datastax.oss.driver.api.core.uuid.Uuids;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
 @Data
-@Table("orders")
+@Document
 public class TacoOrder implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	@PrimaryKey
-	private UUID id = Uuids.timeBased();
+	@Id
+	private String id;
 	
 	private Date placedAt = new Date();
-	
-	@Column("customer_name")
+
 	@NotBlank(message="Delivery name is required")
 	private String deliveryName;
 	
@@ -55,11 +50,10 @@ public class TacoOrder implements Serializable{
  
 	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
+	
+	private List<Taco> tacos = new ArrayList<>();
  
-	@Column("tacos")
-	private List<TacoUDT> tacos = new ArrayList<>();
- 
-	 public void addTaco(TacoUDT taco) {
+	 public void addTaco(Taco taco) {
 		 this.tacos.add(taco);
 	 }
 }
