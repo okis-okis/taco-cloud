46675c774a358beef82450876174e67742901b00
OKIS <67818930+okis-okis@users.noreply.github.com>
2023-08-31 22:54:27 +0300

    Worked on Auth page

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,5 +1,6 @@
 package tacos;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -9,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import  javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
@@ -21,7 +23,10 @@ import lombok.Data;
 
 @Data
 @Entity
-public class TacoOrder {
+public class TacoOrder implements Serializable{
+	
+	@ManyToOne
+	private User user;
 	
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,9 +2,11 @@ package tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import tacos.User;
 import tacos.data.UserRepository;
@@ -23,5 +25,20 @@ public class SecurityConfig {
 	 if (user != null) return user;
 	 throw new UsernameNotFoundException("User ‘" + username + "’ not found");
 	 };
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		return http
+				.authorizeRequests()
+				.antMatchers("/design", "/orders").hasRole("USER")
+				.antMatchers("/", "/**").permitAll()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.and()
+				.logout()
+				.and()
+				.build();
 	}
 }
